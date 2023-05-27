5b0fb0cc25cadce25d25479f9e172d85fe744007
waffle690 <119129001+waffle690@users.noreply.github.com>
2023-05-27 20:42:42 +0300

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- <?php
+function hello($num){
+  $result = ($num * $num * $num);
+  echo $result;
+}
+$num = 5;
+hello($num);
+?> -->
+
+<!-- <?php
+ function func($num) {
+  if ($num >= 0) {
+  return '+';
+  } else {
+   return '-';
+  }
+  }
+  
+ echo func( 3); 
+ echo func(-3); 
+?> -->
+
+
+ <!-- <?php
+function hello1($num, $num1, $num2){
+  $result = ($num + $num1 + $num2);
+  echo $result;
+}
+$num = 5;
+$num1 = 5;
+$num2 = 5;
+hello1($num, $num1, $num2)
+?>  -->
+
+ <!-- <?php
+function hello3($num){
+  $result = ($num * $num * $num);
+  echo $result;
+}
+$num = 34;
+hello3($num);
+?>  -->
+
+<!-- <?php
+$name = "tom";
+$age = 4;
+setcookie("name" , $name);
+setcookie("age" , $age , time ( ) + 3600);
+echo "dd";
+?> -->
+
+<!-- 
+<?php
+  
+  setcookie("user" ,12123,  time())
+
+?> -->
+</body>
+</html>
---
+++ php/index2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <!-- <?php
+
+ if (!isset($_COOKIE['counter'])) {
+     setcookie('counter', 1);
+     $_COOKIE['counter'] = 1;
+    
+    }
+    else{
+        echo "Куки user не найдено";
+    }
+
+   ?> -->
+
+<!-- <?php
+
+if (!isset($_COOKIE['counter'])) {
+    setcookie('counter', time());
+    echo "hl";
+}
+else{
+   echo "gg";
+}
+  ?> -->
+
+</body>
+</html> 
